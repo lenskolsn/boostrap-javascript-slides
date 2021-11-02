806c54d438e9f8b641e7cc0581d9bdbc01faa5d2
lnam6507 <lnam6507@gmail.com>
2021-11-02 19:44:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function menu_mobile(){
+    document.getElementById("menu-mobile").style.display="block";
+    document.getElementById("overflow").style.display="block";
+}
+function tat_menu_mobile(){
+    document.getElementById("menu-mobile").style.display="none";
+    document.getElementById("overflow").style.display="none";
+}
+/*Slides*/
+// var dem = 1;
+// changeimage = function(){
+//     var imgs =["images/hinh1.jpg","images/hinh2.jpg","images/hinh3.jpg","images/hinh4.jpg"];
+//     document.getElementById('img').src= imgs[dem];
+//     dem++;
+//     if(dem==4){
+//         dem=0;
+//     }
+// }
+// setInterval(changeimage,2000);
